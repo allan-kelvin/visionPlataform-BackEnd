43d9978de848fd2451eb67f53a718c406ecc53d6
allan santos <allankelvin.santos@hotmail.com>
2026-04-08 13:43:46 -0300

    criando dto calendar

---
+++ VisionPlatform.Application/DTOs/Dashboard/DashboardCalendarDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VisionPlatform.Application.DTOs.Dashboard
+{
+    public class DashboardCalendarDto
+    {
+        public DateTime Data { get; set; }
+        public string Tipo { get; set; } // Planejamento, Teste, Deploy
+        public int Quantidade { get; set; }
+    }
+}
